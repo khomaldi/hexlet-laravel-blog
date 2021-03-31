cf9a4779bad5f53278b56e6b670d5ae132fbf4a4
Georgii Khomaldishvili <khomaldi@yandex.ru>
2021-03-31 17:07:30 +0300

    filled articles table

--- database/migrations/2021_03_31_134310_create_articles_table.php
+++ database/migrations/2021_03_31_134310_create_articles_table.php
@@ -15,6 +15,10 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('body');
+            $table->string('img_url')->nullable();
+            $table->integer('views_count')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('cojs/app.js') }}"></script>
 </head>
 <body>
 <div class="container mt-4">
