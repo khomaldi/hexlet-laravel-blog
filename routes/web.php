a0132cde09fb4128030dfcc3fc33d823971cc40f
Georgii Khomaldishvili <khomaldi@yandex.ru>
2021-03-31 18:14:33 +0300

    created PageController and moved about view

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    namespace App\Http\Controllers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/about', function () {
-    return view('about');
-});
+    Route::get(
+        '/',
+        function () {
+            return view('welcome');
+        }
+    );
+
+    Route::get('/about', [PageController::class, 'about']);
