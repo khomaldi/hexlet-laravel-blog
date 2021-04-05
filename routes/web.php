d5653a26aaade7ec6efd8b762bf7fbafcda23c3f
Georgii <khomaldi@yandex.ru>
2021-04-05 13:14:37 +0300

    added Article

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@
     );
 
     Route::get('/about', [PageController::class, 'about']);
+
+    // Название сущности в URL во множественном числе, контроллер в единственном
+    Route::get('articles', [ArticleController::class, 'index'])->name(
+            'articles.index'
+        ); // имя маршрута, нужно для того, чтобы не создавать ссылки руками
